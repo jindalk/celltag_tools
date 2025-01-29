6d46fee6ee6fbbac04e8caf61b19dca6db4b3ea9
Kunal Jindal <kjindal@Kunals-MacBook-Air.local>
2025-01-28 20:16:11 -0500

    yaml cmake setup for RTD

--- setup.py
+++ setup.py
@@ -1,18 +1,16 @@
 import os
 from setuptools import setup, find_packages
 
-on_rtd = os.getenv("READTHEDOCS") == "True"
+# on_rtd = os.getenv("READTHEDOCS") == "True"
 
 install_requires=[
         "numpy>=1.21.0",
         "pandas>=1.3.0",
         "matplotlib>=3.4.0",
-        "igraph==0.9.8",
+        "python-igraph==0.9.8",
         "scipy>=1.11.4"
     ]
 
-if not on_rtd:
-    install_requires.append("igraph==0.9.8")
 setup(
     name="celltag_tools",
     version="0.1.0",
