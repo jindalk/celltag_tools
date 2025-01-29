ebffe441b342674c04f6ba429ba1aecf56744981
Kunal Jindal <kjindal@Kunals-MacBook-Air.local>
2025-01-28 20:02:27 -0500

    update setup for RTD

--- setup.py
+++ setup.py
@@ -7,12 +7,11 @@ install_requires=[
         "numpy>=1.21.0",
         "pandas>=1.3.0",
         "matplotlib>=3.4.0",
-        "igraph==0.9.8",
         "scipy>=1.11.4"
     ]
 
 if not on_rtd:
-    install_requires.append("igraph")
+    install_requires.append("igraph==0.9.8")
 setup(
     name="celltag_tools",
     version="0.1.0",
